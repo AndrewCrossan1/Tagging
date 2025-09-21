72309466920d6f8bb4dd55da1fa7449418add4cb
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-09-21 17:33:37 +0100

    fix: Add TJoin for including tags property

--- Live.AndrewCrossan.Tagging/Models/BaseEntity.cs
+++ Live.AndrewCrossan.Tagging/Models/BaseEntity.cs
@@ -6,12 +6,14 @@ namespace Live.AndrewCrossan.Tagging.Models;
 /// This ensures consistency across all entities in the system.
 /// </summary>
 /// <typeparam name="TKey">The Primary Key Type</typeparam>
-public class BaseEntity<TKey>
+public class BaseEntity<TKey, TJoin>
 {
     public TKey Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public bool Active { get; set; }
+    
+    public ICollection<TJoin> Tags { get; set; }
     
     protected BaseEntity()
     {
