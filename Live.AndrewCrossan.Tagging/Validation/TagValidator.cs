e83211ef85c16728b92d8c287c12a8296d549cee
Andrew Crossan <andrew.crossan.2022@uni.strath.ac.uk>
2025-09-27 11:24:43 +0100

    feat: Add default validation class for tags

---
+++ Live.AndrewCrossan.Tagging/Validation/TagValidator.cs
@@ -0,0 +1,40 @@
+﻿using Live.AndrewCrossan.Tagging.Models;
+
+namespace Live.AndrewCrossan.Tagging.Validation;
+
+public class TagValidator<TTag> : ITagValidator<TTag>
+    where TTag : class, ITag, new()
+{
+    public Dictionary<string, List<string>> Errors { get; set; }
+
+    public TagValidator()
+    {
+        Errors = new Dictionary<string, List<string>>();
+    }
+    
+    /// <summary>
+    /// Asynchronously validates the tag.
+    /// This method should be implemented to perform the actual validation logic.
+    /// It should return true if the tag is valid, or false if it is not.
+    /// If the tag is not valid, it should populate the Errors dictionary with appropriate error messages
+    /// for each validation failure.
+    /// </summary>
+    /// <param name="tag">The model to validate</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>True, if the validation is successful, false otherwise</returns>
+    public Task<bool> ValidateAsync(TTag? tag, CancellationToken cancellationToken = default)
+    {
+        // Evaluate each field for null values
+        if (tag == null)
+        {
+            Errors["Tag"] = new List<string> { "Tag cannot be null." };
+        }
+
+        if (String.IsNullOrEmpty(tag.Name))
+        {
+            Errors["Name"] = new List<string> { "Tag name cannot be null." };
+        }
+        
+        return Task.FromResult(Errors.Count == 0);
+    }
+}
